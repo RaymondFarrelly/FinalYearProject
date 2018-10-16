51f0627fde7ea0bce2bdcda150bd67043b4a8c8c
Raymond Farrelly <=rofarrelly@gmail.com>
2018-10-16 18:42:38 +0100

    added some test files

---
+++ Lab1.java
@@ -0,0 +1,30 @@
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
+		q4();
+
+	}
+	public static void q1(){
+		System.out.println("Welcome to CS161");
+	}
+	public static void q2(){
+		System.out.println("Hello\nworld!");
+	}
+	public static void q3(){
+		System.out.println("3+4");
+		System.out.println(3+4);
+		System.out.println(3+"4");
+	}
+	public static void q4(){
+		final int num1=5;
+		final double num2=2.3;
+		final char letter='s';
+		
+		System.out.println(num1+"\n" + num2 +"\n" + letter);
+	}
+	
+}
