fb40185e3ccef510c2956acdc1d01ea0b28bb265
Raymond Farrelly <=rofarrelly@gmail.com>
2018-10-17 09:11:48 +0100

    Added script file and also Fixed solution

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+javac $1.java
+if javac $1.java; then
+    java $1
+    java -jar checkstyle-8.13-all.jar -c google_checks.xml $1.java>output.txt
+    if grep -q "[WARN]" output.txt; then
+        echo "We've got issues"
+    else
+        echo "We have no issues"
+    fi
+else
+    echo "Build fails!"
+fi
