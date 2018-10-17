c2c5f1981d24b55249990c03e8b59b14583e26c2
Raymond Farrelly <=rofarrelly@gmail.com>
2018-10-17 13:10:03 +0100

    Actual fix

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ javac $1.java
 if javac $1.java; then
     java $1
     java -jar checkstyle-8.13-all.jar -c google_checks.xml $1.java > output.txt
-    if grep -q "[WARN]" output.txt; then
+    if grep -q "WARN" output.txt; then
         echo "We've got issues"
     else
         echo "We have no issues"
